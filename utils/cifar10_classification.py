66e936c53807ae9e2a6981de63986157f2bb1e2c
mea <mahmuodimad@gmail.com>
2026-07-01 14:27:13 -0700

    #

--- utils/cifar10_classification.py
+++ utils/cifar10_classification.py
@@ -1,4 +1,5 @@
 import torch 
 
-def train(model, dataloader_train,dataload_val, configs):
+def train(model, dataloader_train,dataload_val, le=1e-3, epochs=100, batch_size=64, patience=100):
     model.train()
+    count = 0

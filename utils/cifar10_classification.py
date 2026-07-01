02377069d02f8b3f56cb4d27854a57a7550bc4e0
mea <mahmuodimad@gmail.com>
2026-07-01 14:38:50 -0700

    #

--- utils/cifar10_classification.py
+++ utils/cifar10_classification.py
@@ -1,6 +1,8 @@
-import torch 
+import torch
 
 def train(model, dataloader_train,dataload_val, le=1e-3, epochs=100, batch_size=64, patience=100):
     model.train()
     count = 0
+    optimizer = torch.optim.SGD() #minibatch
     for image, label in enumerate(dataloader_train):
+        optimizer.zero_grads() #zero out the gradients

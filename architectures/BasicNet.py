d42cb64100e3188bb34e7b422f9d61726b3f0e40
mea <mahmuodimad@gmail.com>
2026-07-01 05:31:30 -0700

    #

--- architectures/BasicNet.py
+++ architectures/BasicNet.py
@@ -2,8 +2,7 @@ from torch import nn
 
 class BasicNet(nn.Module):
     def __init__(self):
-        self.layers=nn.Sequential(nn.Conv2d(batch_size=64, in_channels=3, out_channels=96, 
-            kernel_size=(11,11), stride=4),
+        self.layers=nn.Sequential(nn.Conv2d(in_channels=3, out_channels=96, kernel_size=(11,11), stride=4),
             nn.ReLU(), 
             nn.MaxPool2d((3,3), stride=2),
             nn.Linear(4096,1000),

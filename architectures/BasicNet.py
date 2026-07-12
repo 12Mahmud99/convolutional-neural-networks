56cb73f8604cae6d84a1bbabab25a09b66f8e01c
mea <mahmuodimad@gmail.com>
2026-07-11 20:51:18 -0700

    S

--- architectures/BasicNet.py
+++ architectures/BasicNet.py
@@ -4,10 +4,10 @@ class BasicNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.layers = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=(11,11), stride=4),
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=(11,11), stride=1),
             nn.ReLU(), 
             nn.Flatten(),
-            nn.Linear(3456, 10), 
+            nn.Linear(46464, 10), 
         )
         
     def forward(self, x):

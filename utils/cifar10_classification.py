5e12aae53e57af3e26b6555df44152da470dc979
mea <mahmuodimad@gmail.com>
2026-07-14 02:28:13 -0700

    $

--- utils/cifar10_classification.py
+++ utils/cifar10_classification.py
@@ -10,25 +10,20 @@ def train(model, dataloader_train, dataloader_val=None, epochs=1):
         for batch_index, (image, label) in enumerate(dataloader_train):
             optimizer.zero_grad() 
             output = model(image) 
-            
+
             loss = torch.nn.functional.cross_entropy(output, label) 
             loss.backward()
             optimizer.step()
 
 def test(model, dataset_test):
+    correct_predictions = 0
+    total_samples = dataset_test.dataset.__len__()
+    correct_label_to_prediction_count = {}
     model.eval() 
     for batch_index, (image, label) in enumerate(dataset_test):
         with torch.no_grad():
             output = model(image)
 
-            num_images_to_show = min(10, image.size(0))
-            for i in range(num_images_to_show):
-                img_tensor = image[i].cpu()
-                
-                img_numpy = img_tensor.permute(1, 2, 0).numpy()
-                
-                plt.imshow(img_numpy)
-                plt.axis('off')  
-                plt.show()
-                
-                print(f"Predicted Class: {torch.argmax(output[i]).item()}")
+            correct_predictions += (output.argmax(dim=1) == label).sum().item()
+    
+    return correct_predictions / total_samples

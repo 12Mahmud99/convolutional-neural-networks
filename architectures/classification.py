f4f9e0ff32b931ac4005306d7f4599d5d4733f8c
mea <mahmuodimad@gmail.com>
2026-07-22 22:48:35 -0700

    quick sync

--- architectures/classification.py
+++ architectures/classification.py
@@ -28,13 +28,13 @@ class AlexNet(nn.Module):
             nn.MaxPool(kernel_size=(3,3), stride=2),
             
             #third conv layer
-            nn.Conv2d(in_channels=245, out_channels=384, kernel_size=(3,3), stride=1, padding=1)
+            nn.Conv2d(in_channels=245, out_channels=384, kernel_size=(3,3), stride=1, padding=1),
 
             #fourth conv layer
-            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=(3,3), stride=1, padding=1)
+            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
         
             #fifth conv layer
-            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=(3,3), stride=1, padding=1)                    
+            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=(3,3), stride=1, padding=1),                   
         
         )
     def forward(self, x):

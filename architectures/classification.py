c207e733a1f67ee57dcf8a1ce504ae8cb4a1d21e
mea <mahmuodimad@gmail.com>
2026-07-22 22:39:07 -0700

    ##

--- architectures/classification.py
+++ architectures/classification.py
@@ -20,7 +20,7 @@ class AlexNet(nn.Module):
         super().__init__()
         self.layers=nn.Sequential(
             nn.Conv2d(in_channel=3, out_channels=96, kernel_size=(11,11),stride=4),
-            
+            nn.MaxPool(kernel_size=(3,3),stride=2),
         )
     def forward(self, x):
         

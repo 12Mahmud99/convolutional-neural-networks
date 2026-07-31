2d861a05a82d4b2a2b22fca14bc7b237ca59cc6f
mea <mahmuodimad@gmail.com>
2026-07-30 23:00:42 -0700

    #

--- architectures/classification.py
+++ architectures/classification.py
@@ -75,7 +75,7 @@ class AlexNet32(nn.Module):
         super().__init__()        
         self.layers=nn.Sequential(
             #first conv layer
-            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=(11,11),stride=4, padding=0),
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=(2,2),stride=4, padding=0),
             nn.ReLU(),
             nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
             nn.MaxPool2d(kernel_size=(3,3),stride=2),

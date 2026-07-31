fb25e65d3162b9c4ff7e224e653b8b39dcf052f2
mea <mahmuodimad@gmail.com>
2026-07-30 23:12:03 -0700

    #

--- architectures/classification.py
+++ architectures/classification.py
@@ -96,8 +96,7 @@ class AlexNet32(nn.Module):
             #fifth conv layer
             nn.Conv2d(in_channels=384, out_channels=256, kernel_size=(3,3), stride=1, padding=1),                   
             nn.ReLU(),
-            
-            #TODO function of localResponseNorm
+
             nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
             nn.MaxPool2d(kernel_size=(3,3),stride=2),
             

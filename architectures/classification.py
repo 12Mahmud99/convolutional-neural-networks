1789567da30b91338deadaa330628562e401fdb5
mea <mahmuodimad@gmail.com>
2026-07-31 23:23:36 -0700

    #

--- architectures/classification.py
+++ architectures/classification.py
@@ -86,7 +86,7 @@ class AlexNet32(nn.Module):
             nn.MaxPool2d(kernel_size=(3,3), stride=2),
             
             #third conv layer
-            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=(1,1), stride=1, padding=1),
+            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=(2,2), stride=1, padding=1),
             nn.ReLU(),
             
             #fourth conv layer

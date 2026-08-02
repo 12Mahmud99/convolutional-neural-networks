b75a1e44ffd608d74b94f2b69df06399c17eb484
mea <mahmuodimad@gmail.com>
2026-08-01 23:12:54 -0700

    #

--- architectures/classification.py
+++ architectures/classification.py
@@ -4,14 +4,12 @@ from torch import nn
 class BasicNet(nn.Module):
     def __init__(self, pooling="max"):
         super().__init__()
-
         self.layers = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=96, kernel_size=(11,11), stride=1),
             nn.ReLU(), 
             nn.Flatten(),
             nn.Linear(46464, 10), 
         )
-        
     def forward(self, x):
         return self.layers(x)
 
@@ -131,12 +129,16 @@ class VGG16(nn.Module):
 
 ##BasicResNet
 class BasicResNet(nn.Module):
-    def __init__(self):
-        super.__init__()
-        pass
-    
-    def forward(x,self):
-        pass 
+    def __init__(self, pooling="max"):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=(11,11), stride=1),
+            nn.ReLU(), 
+            nn.Flatten(),
+            nn.Linear(46464, 10), 
+        )
+    def forward(self, x):
+        return self.layers(x)
 
 ##ResNet-50
 class ResNet50(nn.Module):
